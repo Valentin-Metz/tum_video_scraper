25e04d3812024c849bc2fe4a353439df6d1de3b8
Frank Elsinga <frank@elsinga.de>
2021-03-18 19:16:11 +0100

    implemented proposed improvements: plus-string -> f-string set->Typing alias spawining loads of processes-> ProcessPool-ExecuterService yt-download->ffmpeg for speed and dependency reasons

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import os
 import re
-from multiprocessing import Process
+import tempfile
+import time
+from multiprocessing import Pool
+from os.path import expanduser
+from typing import Set
 from urllib.parse import urlparse, urljoin
 
 import requests
 from bs4 import BeautifulSoup
 
-source_url = 'https://live.rbg.tum.de/cgi-bin/streams'
-destination_folder_path = '/home/feuermagier/Videos/Lectures/'
-tmp_directory = '/tmp/tum_video_scraper/'
-
-os.system("pip3 install -U auto-editor")  # update auto-editor because it is constantly out of date
-if not os.path.isdir(tmp_directory):  # create temporary work-directory
-    os.mkdir(tmp_directory)
+SOURCE_URL = 'https://live.rbg.tum.de/cgi-bin/streams'
+DESTINATION_FOLDER_PATH = os.path.join(expanduser("~"), "Videos", "Lectures")
+TMP_DIRECTORY = os.path.join(tempfile.gettempdir(), "tum_video_scraper")
+if not os.path.isdir(TMP_DIRECTORY):  # create temporary work-directory
+    os.mkdir(TMP_DIRECTORY)
+PROCESS_COUNT = os.cpu_count()
 
 
 def is_valid_url(url: str) -> bool:
@@ -21,7 +24,7 @@ def is_valid_url(url: str) -> bool:
 
 
 # source: https://www.thepythoncode.com/article/extract-all-website-links-python
-def get_all_website_links(url: str) -> set[str]:
+def get_all_website_links(url: str) -> Set[str]:
     urls_on_site = set()
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
     for a_tag in soup.findAll("a"):
@@ -33,7 +36,7 @@ def get_all_website_links(url: str) -> set[str]:
         href = urljoin(url, href)
         parsed_href = urlparse(href)
         # remove URL GET parameters, URL fragments, etc.
-        href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
+        href = f"{parsed_href.scheme}://{parsed_href.netloc}{parsed_href.path}"
         if not is_valid_url(href):
             # not a valid URL
             continue
@@ -44,61 +47,68 @@ def get_all_website_links(url: str) -> set[str]:
     return urls_on_site
 
 
-def filter_urls(urls: set[str]) -> set[str]:  # filters for combined view only
+def filter_urls(urls: Set[str]) -> Set[str]:  # filters for combined view only
     return {url for url in urls if url.endswith("/COMB")}
 
 
-def get_videos_of_subject(urls: set[str], name: str, url_identifier: str):
+def get_videos_of_subject(urls: Set[str], name: str, url_identifier: str):
     """The URL-Identifier is found in the URL of every video of the target subject"""
-    output_folder_path = os.path.join(destination_folder_path, name)
+
+    output_folder_path = os.path.join(DESTINATION_FOLDER_PATH, name)
     if not os.path.isdir(output_folder_path):
         os.mkdir(output_folder_path)
     urls = {url for url in urls if url_identifier in url}
+    args_list = []
     for url in urls:
-        filename = get_name(url) + ".mp4"
+        filename = f"{get_name(url)}.mp4"
         playlist_url = get_playlist_url(url)
         output_file_path = os.path.join(output_folder_path, filename)
-        """We use locks to prevent processing the same video twice (for example if we run as a daemon)
-        Locks can also be created by the user to keep us from downloading a specific video"""
-        if not (os.path.isfile(output_file_path + ".lock")  # check if lock exists
-                or os.path.isfile(output_file_path)):  # check if file exists
-            open(os.path.join(output_file_path + ".lock"), 'a')  # create lock
-            """We use one process for every video"""
-            #    download_and_cut(playlist_url, filename, output_file_path)
-            Process(target=download_and_cut, args=(playlist_url, filename, output_file_path)).start()
+        # We use locks to prevent processing the same video twice (for example if we run as a daemon)
+        # Locks can also be created by the user to keep us from downloading a specific video"""
+        if not os.path.isfile(f"{output_file_path}.lock") and not os.path.isfile(output_file_path):
+            args_list.append((playlist_url, filename, output_file_path))
+    # download_and_cut(playlist_url, filename, output_file_path)
+    # We use one process of the Processpool for every video
+    process_pool = Pool(PROCESS_COUNT)
+    process_pool.starmap(download_and_cut, args_list)
 
 
 def download_and_cut(playlist_url: str, filename: str, output_file_path: str):
-    temporary_path = tmp_directory + filename + ".original"
-    download_command = "youtube-dl " + playlist_url + " -o " + temporary_path
-    print("Starting download of " + filename)
-    os.system(download_command)
-    cut_command = "auto-editor " + temporary_path + " --silent_speed 8 --no_open -o " + output_file_path
-    print("Starting to convert " + filename)
-    os.system(cut_command)
-    os.remove(temporary_path)
-    os.remove(output_file_path + ".lock")  # remove lock
-    print("Done with " + filename)
+    if not os.path.isfile(f"{output_file_path}.lock") and not os.path.isfile(output_file_path):
+        open(os.path.join(f"{output_file_path}.lock"), 'a')  # create lock
+
+        start_time = time.time()
+        with tempfile.NamedTemporaryFile(delete=True, dir=TMP_DIRECTORY, suffix=".mp4") as temporary_path:
+            print(f"Starting download of {filename}")
+            os.system(f"ffmpeg -y -hide_banner -hwaccel auto -loglevel warning "
+                      f"-i {playlist_url} "
+                      f"-c copy "
+                      f"{temporary_path.name}")
+            print(f"Starting to convert {filename}")
+            os.system(f"auto-editor {temporary_path.name} "
+                      f"--silent_speed 8 --frame_margin 15 --video_codec libx264 --constant_rate_factor 30 --no_progress --no_open "
+                      f"-o {output_file_path}")
+
+        os.remove(f"{output_file_path}.lock")  # remove lock
+        print(f"Done with {filename} in {str(time.time() - start_time)}s")
 
 
 def get_name(url: str) -> str:
-    name = url.rsplit('/')[-2]
-    return name
+    return url.rsplit('/')[-2]
 
 
 def get_playlist_url(url: str) -> str:
     text = requests.get(url).text
     prefix = 'https://stream.lrz.de/vod/_definst_/mp4:tum/RBG'
     postfix = '.mp4/playlist.m3u8'
-    playlist_extracted_url = re.search(prefix + '(.+?)' + postfix, text).group(1)
-    playlist_url = prefix + playlist_extracted_url + postfix
-    return playlist_url
+    playlist_extracted_url = re.search(f'{prefix}(.+?){postfix}', text).group(1)
+    return prefix + playlist_extracted_url + postfix
 
 
 if __name__ == '__main__':
     os.nice(15)  # set our nice value, so we can work in the background
-    all_urls = get_all_website_links(source_url)
+    all_urls = get_all_website_links(SOURCE_URL)
     video_urls = filter_urls(all_urls)
-
+    # download_and_cut('https://stream.lrz.de/vod/_definst_/mp4:tum/RBG/WiSe2021GBS20201221134500.13.009ACOMB.mp4/playlist.m3u8','2020_12_21_13_45.mp4','/home/frank/Videos/Lectures/GBS/2020_12_21_13_45.mp4')
     get_videos_of_subject(video_urls, 'GBS', 'WiSe2021GBS')
-    get_videos_of_subject(video_urls, 'NumProg', 'WiSe2021NumProg')
+    # get_videos_of_subject(video_urls, 'NumProg', 'WiSe2021NumProg')
