fe54468e92315b84cf5539fc9763d33af3db28e7
Valentin Metz <Valentin.Metz@web.de>
2023-05-09 13:25:22 +0200

    Adapted panopto tests

--- test/test_panopto.py
+++ test/test_panopto.py
@@ -46,7 +46,6 @@ def test_get_folders_fast():
 def test_get_folders_large():
     panopto_folders = {
         'Studentische Vollversammlung 950633228 (W22/23)': '81daaf4e-cbdd-42c6-8a37-af4c0100c518',
-        'Algorithmic Game Theory 2021': '56d3768c-f81c-45ea-9949-ad0500e7a73b',
         'Introduction to Biological Imaging 950567672 (W21/22)': 'b87a3aa9-6f30-4fe3-9804-adcc00c7b486',
         'Maschinelles Lernen 950631887 (W22/23)': '37c7610b-63e6-46a2-b403-af2f0123f5c0',
     }
@@ -55,6 +54,5 @@ def test_get_folders_large():
     panopto.get_folders(panopto_folders, os.environ['TUM_USERNAME'], os.environ['TUM_PASSWORD'], videos_for_subject)
 
     assert (len(videos_for_subject['Studentische Vollversammlung 950633228 (W22/23)']) == 1)
-    assert (len(videos_for_subject['Algorithmic Game Theory 2021']) == 38)
     assert (len(videos_for_subject['Introduction to Biological Imaging 950567672 (W21/22)']) == 19)
     assert (len(videos_for_subject['Maschinelles Lernen 950631887 (W22/23)']) == 55)
